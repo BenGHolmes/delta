7320b0ec3842ccda1024bf11715ec8300c72f619
Ben Holmes <7benholmes@gmail.com>
2023-12-01 01:17:58 -0800

    second write

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@ use polars::prelude::*;
 
 fn main() -> Result<(), DeltaError> {
     let table = DeltaTable::create_table("my-table", vec![("foo", "int"), ("bar", "TEXT")])?;
+
     table.insert(vec![
         vec!["1", "test row"],
         vec!["2", "another test row"],
         vec!["3", "the final row"],
     ])?;
 
+    table.insert(vec![vec!["4", "second insert"]])?;
+
     let df = LazyFrame::scan_parquet(
         "tables/my-table/00000000000000000000.parquet",
         Default::default(),
     )?
     .collect()?;
+    println!("{}", df);
 
+    let df = LazyFrame::scan_parquet(
+        "tables/my-table/00000000000000000001.parquet",
+        Default::default(),
+    )?
+    .collect()?;
     println!("{}", df);
 
     Ok(())
